5c34cb19f7258b1eebccfbb790c1605a32d174b9
clstyn <fiorenzacelestyn@gmail.com>
2023-07-11 23:09:59 +0700

    feat: page 404

--- src/pages/Page404.jsx
+++ src/pages/Page404.jsx
@@ -1,5 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Gambar from "../assets/images/image404.png";
 
 export const Page404 = () => {
-  return <div>This Page Not Found</div>;
+  return (
+    <div className="w-screen min-h-screen bg-c-dark-green text-c-light-cream flex flex-col items-center justify-center font-poppins">
+      <img src={Gambar} alt="404" className="my-4 md:my-12 max-md:w-1/2" />
+      <h1 className="text-xl sm:text-3xl lg:text-4xl font-semibold">
+        Halaman tidak ditemukan
+      </h1>
+      <Link to="/" className="text-lg lg:text-xl my-4 md:my-12 hover:underline">
+        Kembali ke Beranda
+      </Link>
+    </div>
+  );
 };
