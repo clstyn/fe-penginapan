bcaec922f1a265fdd71a9ec98244539ba3916e32
clstyn <fiorenzacelestyn@gmail.com>
2023-07-12 22:12:43 +0700

    feat: modal add and edit penginapan

--- src/components/CardMyProperty.jsx
+++ src/components/CardMyProperty.jsx
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
+import { Transition } from "@headlessui/react";
 import { FaEdit } from "react-icons/fa";
 import { RiDeleteBinLine } from "react-icons/ri";
 
 export const CardMyProperty = (props) => {
+  const [isEditModalOpen, setIsEditModalOpen] = useState(false);
+
+  const openModal = () => {
+    setIsEditModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsEditModalOpen(false);
+  };
+
   return (
     <div className="w-full h-fit min-h-[300px] flex max-md:flex-col rounded-xl shadow-lg">
       <img
@@ -17,7 +28,7 @@ export const CardMyProperty = (props) => {
           </p>
           <div className="flex gap-1">
             <div className="cursor-pointer w-10 aspect-square bg-orange-400 hover:bg-orange-500 rounded-lg flex items-center justify-center text-white">
-              <FaEdit className="text-xl" />
+              <FaEdit onClick={openModal} className="text-xl" />
             </div>
             <div className="cursor-pointer w-10 aspect-square bg-red-500 hover:bg-red-600 rounded-lg flex items-center justify-center text-white">
               <RiDeleteBinLine className="text-xl" />
@@ -61,6 +72,134 @@ export const CardMyProperty = (props) => {
           </span>
         </p>
       </div>
+
+      {/* Modal */}
+
+      <Transition
+        show={isEditModalOpen}
+        as={React.Fragment}
+        enter="transition-opacity ease-out duration-300"
+        enterFrom="opacity-0"
+        enterTo="opacity-100"
+        leave="transition-opacity ease-in duration-200"
+        leaveFrom="opacity-100"
+        leaveTo="opacity-0"
+      >
+        <div className="fixed inset-0 flex items-center justify-center z-50">
+          <div className="absolute inset-0 bg-black opacity-50"></div>
+
+          <div className="bg-c-light-cream rounded-lg p-8 w-3/4 md:w-1/2 z-50">
+            <h2 className="text-xl font-semibold mb-4">Ubah Properti</h2>
+
+            {/* Add Penginapan Form */}
+            {/* Replace with your form fields and styling */}
+            <form>
+              <div className="mb-4">
+                <label htmlFor="namaKost" className="block mb-1">
+                  Nama Properti
+                </label>
+                <input
+                  type="text"
+                  id="namaKost"
+                  className="border border-gray-300 px-4 py-2 w-full rounded"
+                  placeholder="Nama Kost/Homestay/Penginapan"
+                />
+              </div>
+              <div className="mb-4">
+                <label htmlFor="hargaPerMonth" className="block mb-1">
+                  Harga/Rate
+                </label>
+                <input
+                  type="text"
+                  id="hargaPerMonth"
+                  className="border border-gray-300 px-4 py-2 w-full rounded"
+                  placeholder="Harga Sewa per Bulan"
+                />
+              </div>
+              <div className="flex gap-2">
+                <div className="mb-4 w-1/2">
+                  <label htmlFor="totalRoom" className="block mb-1">
+                    Total Kamar
+                  </label>
+                  <input
+                    type="number"
+                    id="totalRoom"
+                    className="border border-gray-300 px-4 py-2 w-full rounded"
+                    placeholder="Jumlah total kamar"
+                  />
+                </div>
+                <div className="mb-4 w-1/2">
+                  <label htmlFor="bookedRoom" className="block mb-1">
+                    Kamar Terisi
+                  </label>
+                  <input
+                    type="number"
+                    id="bookedRoom"
+                    className="border border-gray-300 px-4 py-2 w-full rounded"
+                    placeholder="Jumlah kamar yang terisi"
+                  />
+                </div>
+              </div>
+              <div className="mb-4">
+                <label htmlFor="phoneNo" className="block mb-1">
+                  No. Telepon
+                </label>
+                <input
+                  type="text"
+                  id="phoneNo"
+                  className="border border-gray-300 px-4 py-2 w-full rounded"
+                  placeholder="Nomor telepon yang terhubung dengan WhatsApp"
+                />
+              </div>
+              <div className="mb-4">
+                <label htmlFor="fasilitas" className="block mb-1">
+                  Fasilitas
+                </label>
+                <input
+                  type="text"
+                  id="fasilitas"
+                  className="border border-gray-300 px-4 py-2 w-full rounded"
+                  placeholder="Sebutkan fasilitas yang tersedia. Dipisahkan dengan koma. Misalnya AC, Wi-Fi, Kamar Mandi Dalam"
+                />
+              </div>
+              <div className="mb-4">
+                <label htmlFor="imgUrl" className="block mb-1">
+                  Unggah Gambar (Pilih 1 gambar)
+                </label>
+                <input
+                  type="file"
+                  id="imgUrl"
+                  className="border border-gray-300 px-4 py-2 w-full rounded"
+                />
+              </div>
+              <div className="mb-4">
+                <label htmlFor="map" className="block mb-1">
+                  Pilih Lokasi
+                </label>
+                <div
+                  id="map"
+                  className="border border-gray-300 px-4 py-2 w-full h-[150px] rounded"
+                ></div>
+              </div>
+              <div className="flex justify-end mt-8">
+                <button
+                  type="button"
+                  className="mr-2 bg-red-500 hover:bg-red-600 text-white font-semibold py-2 px-4 rounded"
+                  onClick={closeModal}
+                >
+                  Batal
+                </button>
+                <button
+                  type="submit"
+                  className="bg-c-mid-green hover:bg-c-dark-green text-white font-semibold py-2 px-4 rounded"
+                >
+                  Simpan
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </Transition>
     </div>
   );
 };
--- src/pages/MyProperty.jsx
+++ src/pages/MyProperty.jsx
@@ -1,10 +1,26 @@
 import React, { useEffect, useState } from "react";
+import { Transition } from "@headlessui/react";
+
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
 
 import { NavbarAfterLogin } from "../components/navbar/NavbarAfterLogin";
 import { CardMyProperty } from "../components/CardMyProperty";
 
 export const MyProperty = () => {
   const [myProperty, setMyProperty] = useState([]);
+  const [location, setLocation] = useState([]);
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
   const MyPropertyDummy = [
     {
       namaKost: "Nama Kost 2",
@@ -38,7 +54,20 @@ export const MyProperty = () => {
 
   useEffect(() => {
     setMyProperty(MyPropertyDummy);
-  }, []);
+  }, [MyPropertyDummy]);
+
+  // useEffect(() => {
+  //   var map = L.map("map").setView([-7.894894, 110.061906], 15);
+  //   var mapLink = '<a href="http://openstreetmap.org">OpenStreetMap</a>';
+  //   L.tileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  //     attribution: "&copy; " + mapLink + " Contributors",
+  //     maxZoom: 18,
+  //   }).addTo(map);
+
+  //   return () => {
+  //     map.remove();
+  //   };
+  // }, [setIsModalOpen]);
 
   return (
     <div className="bg-c-light-cream font-poppins">
@@ -51,7 +80,10 @@ export const MyProperty = () => {
           <p className="text-lg md:text-xl xl:text-2xl">
             Kelola bisnis kost atau penginapan Anda disini!
           </p>
-          <div className="rounded-xl bg-c-mid-green text-white cursor-pointer px-4 md:px-8 py-2 md:py-4 mt-4 w-fit text-lg md:text-xl mb-8">
+          <div
+            onClick={openModal}
+            className="rounded-xl bg-c-mid-green text-white cursor-pointer px-4 md:px-8 py-2 md:py-4 mt-4 w-fit text-lg md:text-xl mb-8"
+          >
             Tambah Properti
           </div>
           <div className="flex flex-col gap-8 mb-12 md:mb-16 2xl:mb-24">
@@ -61,6 +93,132 @@ export const MyProperty = () => {
           </div>
         </div>
       </div>
+
+      <Transition
+        show={isModalOpen}
+        as={React.Fragment}
+        enter="transition-opacity ease-out duration-300"
+        enterFrom="opacity-0"
+        enterTo="opacity-100"
+        leave="transition-opacity ease-in duration-200"
+        leaveFrom="opacity-100"
+        leaveTo="opacity-0"
+      >
+        <div className="fixed inset-0 flex items-center justify-center z-50">
+          <div className="absolute inset-0 bg-black opacity-50"></div>
+
+          <div className="bg-c-light-cream rounded-lg p-8 w-3/4 md:w-1/2 z-50">
+            <h2 className="text-xl font-semibold mb-4">Tambah Properti</h2>
+
+            {/* Add Penginapan Form */}
+            {/* Replace with your form fields and styling */}
+            <form>
+              <div className="mb-4">
+                <label htmlFor="namaKost" className="block mb-1">
+                  Nama Properti
+                </label>
+                <input
+                  type="text"
+                  id="namaKost"
+                  className="border border-gray-300 px-4 py-2 w-full rounded"
+                  placeholder="Nama Kost/Homestay/Penginapan"
+                />
+              </div>
+              <div className="mb-4">
+                <label htmlFor="hargaPerMonth" className="block mb-1">
+                  Harga/Rate
+                </label>
+                <input
+                  type="text"
+                  id="hargaPerMonth"
+                  className="border border-gray-300 px-4 py-2 w-full rounded"
+                  placeholder="Harga Sewa per Bulan"
+                />
+              </div>
+              <div className="flex gap-2">
+                <div className="mb-4 w-1/2">
+                  <label htmlFor="totalRoom" className="block mb-1">
+                    Total Kamar
+                  </label>
+                  <input
+                    type="number"
+                    id="totalRoom"
+                    className="border border-gray-300 px-4 py-2 w-full rounded"
+                    placeholder="Jumlah total kamar"
+                  />
+                </div>
+                <div className="mb-4 w-1/2">
+                  <label htmlFor="bookedRoom" className="block mb-1">
+                    Kamar Terisi
+                  </label>
+                  <input
+                    type="number"
+                    id="bookedRoom"
+                    className="border border-gray-300 px-4 py-2 w-full rounded"
+                    placeholder="Jumlah kamar yang terisi"
+                  />
+                </div>
+              </div>
+              <div className="mb-4">
+                <label htmlFor="phoneNo" className="block mb-1">
+                  No. Telepon
+                </label>
+                <input
+                  type="text"
+                  id="phoneNo"
+                  className="border border-gray-300 px-4 py-2 w-full rounded"
+                  placeholder="Nomor telepon yang terhubung dengan WhatsApp"
+                />
+              </div>
+              <div className="mb-4">
+                <label htmlFor="fasilitas" className="block mb-1">
+                  Fasilitas
+                </label>
+                <input
+                  type="text"
+                  id="fasilitas"
+                  className="border border-gray-300 px-4 py-2 w-full rounded"
+                  placeholder="Sebutkan fasilitas yang tersedia. Dipisahkan dengan koma. Misalnya AC, Wi-Fi, Kamar Mandi Dalam"
+                />
+              </div>
+              <div className="mb-4">
+                <label htmlFor="imgUrl" className="block mb-1">
+                  Unggah Gambar (Pilih 1 gambar)
+                </label>
+                <input
+                  type="file"
+                  id="imgUrl"
+                  className="border border-gray-300 px-4 py-2 w-full rounded"
+                />
+              </div>
+              <div className="mb-4">
+                <label htmlFor="map" className="block mb-1">
+                  Pilih Lokasi
+                </label>
+                <div
+                  id="map"
+                  className="border border-gray-300 px-4 py-2 w-full h-[150px] rounded"
+                ></div>
+              </div>
+              <div className="flex justify-end mt-8">
+                <button
+                  type="button"
+                  className="mr-2 bg-red-500 hover:bg-red-600 text-white font-semibold py-2 px-4 rounded"
+                  onClick={closeModal}
+                >
+                  Batal
+                </button>
+                <button
+                  type="submit"
+                  className="bg-c-mid-green hover:bg-c-dark-green text-white font-semibold py-2 px-4 rounded"
+                >
+                  Tambahkan
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </Transition>
     </div>
   );
 };
