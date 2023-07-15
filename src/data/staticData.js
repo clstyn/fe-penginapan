244acfde5678ea5eab10c14190f7f25e6f65a5d0
clstyn <fiorenzacelestyn@gmail.com>
2023-07-15 21:39:41 +0700

    add data kost

--- src/data/staticData.js
+++ src/data/staticData.js
@@ -1,12 +1,26 @@
 const staticData = [
   {
-    namaKost: "Nama Kost 2",
-    hargaPerMonth: 1200000,
+    namaKost: "Alsya Kost",
+    hargaPerMonth: 600000,
     totalRoom: 10,
     bookedRoom: 1,
     fasilitas: ["AC", "Wi-Fi"],
     imgUrl:
-      "https://firebasestorage.googleapis.com/v0/b/palihan-penginapan-kkn.appspot.com/o/files%2Fcoba%2F3922687.jpg?alt=media",
+      "https://firebasestorage.googleapis.com/v0/b/palihan-penginapan-kkn.appspot.com/o/files%2FstaticImages%2Falsya.png?alt=media",
+    phoneNo: "081395081155",
+    location: {
+      type: "Point",
+      coordinates: [110.0560303, -7.8947921],
+    },
+  },
+  {
+    namaKost: "AB Kost",
+    hargaPerMonth: 0,
+    totalRoom: 10,
+    bookedRoom: 1,
+    fasilitas: ["AC", "Wi-Fi"],
+    imgUrl:
+      "https://firebasestorage.googleapis.com/v0/b/palihan-penginapan-kkn.appspot.com/o/files%2FstaticImages%2Fab%20kost.png?alt=media",
     phoneNo: "081234567890",
     location: {
       type: "Point",
@@ -14,13 +28,27 @@ const staticData = [
     },
   },
   {
-    namaKost: "Nama Kost 3",
-    hargaPerMonth: 1500000,
+    namaKost: "Kost Pak Muhajir",
+    hargaPerMonth: 500000,
+    totalRoom: 10,
+    bookedRoom: 1,
+    fasilitas: ["AC", "Wi-Fi"],
+    imgUrl:
+      "https://firebasestorage.googleapis.com/v0/b/palihan-penginapan-kkn.appspot.com/o/files%2FstaticImages%2Fmuhajir.png?alt=media",
+    phoneNo: "081234567890",
+    location: {
+      type: "Point",
+      coordinates: [110.052031, -7.891401],
+    },
+  },
+  {
+    namaKost: "Kost Pak Kusmono",
+    hargaPerMonth: 500000,
     totalRoom: 12,
     bookedRoom: 2,
     fasilitas: ["AC", "TV", "Wi-Fi", "AC", "TV", "Wi-Fi", "AC", "TV", "Wi-Fi"],
     imgUrl:
-      "https://firebasestorage.googleapis.com/v0/b/palihan-penginapan-kkn.appspot.com/o/files%2Fcoba%2F3922687.jpg?alt=media",
+      "https://firebasestorage.googleapis.com/v0/b/palihan-penginapan-kkn.appspot.com/o/files%2FstaticImages%2Fkusmono.png?alt=media",
     phoneNo: "081234567891",
     location: {
       type: "Point",
